e6a2c6d7ce2274574734e011b674af07e5e2ca27
Tira <217bssr@gmail.com>
2018-01-20 06:00:33 +0000

    いったんローカル環境に移動する用

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,6 +1,7 @@
 class MenusController < ApplicationController
+    include MenusHelper
     def index
-        
+        doc = getPage
     end
     
 end
--- app/helpers/menus_helper.rb
+++ app/helpers/menus_helper.rb
@@ -1,2 +1,31 @@
+require 'open-uri'
+require 'nokogiri'
+require 'csv'
+
 module MenusHelper
+    def getPage
+#        uri = 'http://www.yahoo.co.jp/'
+#        doc = Nokogiri::HTML(open(uri), nil, "utf-8")
+#        doc = Nokogiri::HTML.parse(open(uri), nil, "utf-8")
+        # スクレイピング先のURL
+#        url = 'http://matome.naver.jp/tech'
+        url = "http://www.tamagoya.co.jp/menu_list.html"
+        charset = nil
+        html = open(url) do |f|
+          charset = f.charset # 文字種別を取得
+          f.read # htmlを読み込んで変数htmlに渡す
+        end
+        # htmlをパース(解析)してオブジェクトを作成
+        doc = Nokogiri::HTML.parse(html, nil, 'utf-8')
+        byebug
+        doc2 = doc.css("div#customscroll")
+        doc.xpath('//li[@class="mdTopMTMList01Item"]').each do |node|
+          # tilte
+          p node.css('h3').inner_text
+          # 記事のサムネイル画像
+          p node.css('img').attribute('src').value
+          # 記事のサムネイル画像
+          p node.css('a').attribute('href').value
+        end
+    end
 end
